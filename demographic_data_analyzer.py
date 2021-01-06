05d00b0b7dfdbfaade8844a42dfacfb9f0270831
adamstok <adamstokowski@outlook.com>
2021-01-06 22:23:19 +0100

    round it!

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -18,7 +18,8 @@ def calculate_demographic_data(print_data=True):
     average_age_men = round(df[df['sex'] == 'Male']['age'].mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = len(df) / len(df[df['education'] == 'Bachelors'])
+    percentage_bachelors = round((
+        len(df[df['education'] == 'Bachelors']) / len(df)) * 100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
